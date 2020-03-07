32700dc131de90b4134b35939edbd47b927cfd54
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-03-07 12:19:10 -0800

    Added some basic controlled form state manipulation on the Event Search Filter component

--- src/components/EventSearchFilter.js
+++ src/components/EventSearchFilter.js
@@ -15,49 +15,53 @@ const EventSearchFilter = props => {
       <form>
         <div className="flex flex-row justify-between pb-1">
           <div>
-            <label className="block text-blue-800" for="inline-full-name">
+            <label className="block text-blue-800 pl-1" for="inline-full-name">
               Event Name
             </label>
             <input
-              className="bg-gray-200 appearance-none rounded"
+              className="bg-gray-200 appearance-none rounded pl-1"
               id="event-name"
               type="text"
               value={eventName}
+              onChange={(e)=>{setEventName(e.target.value)}}
             />
           </div>
           <div>
-            <label className="block text-blue-800" for="inline-full-name">
+            <label className="block text-blue-800 pl-1" for="inline-full-name">
               Nonprofit
             </label>
             <input
-              className="bg-gray-200 appearance-none rounded"
+              className="bg-gray-200 appearance-none rounded pl-1"
               id="nonprofit"
               type="text"
               value={nonprofitName}
+              onChange={(e)=>{setNonprofitName(e.target.value)}}
             />
           </div>
         </div>
         <div className="flex flex-row justify-between">
         <div>
-            <label className="block text-blue-800" for="inline-full-name">
+            <label className="block text-blue-800 pl-1" for="inline-full-name">
               Start Date
             </label>
             <input
-              className="bg-gray-200 appearance-none rounded"
+              className="bg-gray-200 appearance-none rounded pl-1"
               id="start-date"
               type="text"
               value={eventStartDate}
+              onChange={(e)=>{setEventStartDate(e.target.value)}}
             />
           </div>
           <div>
-            <label className="block text-blue-800" for="inline-full-name">
+            <label className="block text-blue-800 pl-1" for="inline-full-name">
               End Date
             </label>
             <input
-              className="bg-gray-200 appearance-none rounded"
+              className="bg-gray-200 appearance-none rounded pl-1"
               id="end-date"
               type="text"
               value={eventEndDate}
+              onChange={(e)=>{setEventEndDate(e.target.value)}}
             />
           </div>
         </div>
