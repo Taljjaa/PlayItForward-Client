275146f2973bfddc472d569505be7d63348ccbe1
Taljjaa <jenken131@gmail.com>
2020-03-13 10:36:05 -0400

    added apollo provider to app instead of index, works the same way just common practice to have it in App

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,27 @@ import LandingPage from './pages/LandingPage';
 import EventListPage from './pages/EventListPage';
 import NonprofitListPage from './pages/NonprofitListPage';
 
+//Imports from Apollo
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+//the client represents the endpoint to our graphql server
+
+const client = new ApolloClient({
+   uri: 'https://play-it-forward-graphql.herokuapp.com/v1/graphql',
+});
+
+//ApolloProvider allows us to access the client from anywhere in the component tree
+
 // This is the main app component
 function App() {
    return (
       <Router>
-         <div className="bg-blue-200 text-blue-900">
-            <LandingPage />
-         </div>
+         <ApolloProvider client={client}>
+            <div className="bg-blue-200 text-blue-900">
+               <LandingPage />
+            </div>
+         </ApolloProvider>
       </Router>
    );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,22 +4,5 @@ import './styles.css'; // <- change './index.css' to './styles.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-//Imports from Apollo
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
-
-//the client represents the endpoint to our graphql server
-
-const client = new ApolloClient({
-   uri: 'https://play-it-forward-graphql.herokuapp.com/v1/graphql',
-});
-
-//ApolloProvider allows us to access the client from anywhere in the component tree
-
-ReactDOM.render(
-   <ApolloProvider client={client}>
-      <App />
-   </ApolloProvider>,
-   document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
