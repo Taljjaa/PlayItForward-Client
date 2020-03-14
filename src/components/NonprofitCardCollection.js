ac5d631179b7d5c291c71001c913714ef4623d56
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-03-14 12:59:37 -0700

    wrapped up links and added comments

--- src/components/NonprofitCardCollection.js
+++ src/components/NonprofitCardCollection.js
@@ -10,7 +10,7 @@ import {nonprofitData} from '../pages/mock-data'
 // Nonprofit Cards
 const NonprofitCardCollection = (props) => {
     return (
-        <div className="flex flex-col flex-grow items-center w-full">
+        <div className="flex flex-col flex-grow items-center w-full overflow-auto">
             {nonprofitData.nonprofits.map(el => {
                 return <NonprofitCard key={Math.random()} data={el} />
             })}
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,35 +1,58 @@
 // React imports
-import React from 'react';
+import React from "react";
 
 // Custom imports
-import Navbar from '../components/Navbar'
-import newsImage from '../media/animations/news-bird.gif'
-import contributeImage from '../media/animations/contribute-piggy.gif'
-import GifButton from '../components/GifButton'
+import Navbar from "../components/Navbar";
+import newsImage from "../media/animations/news-bird.gif";
+import contributeImage from "../media/animations/contribute-piggy.gif";
+import GifButton from "../components/GifButton";
 
+// Function component that displays general info about the program
+// Layout:
+// Navbar, Text Blurb, GifButtons
 const AboutPage = () => {
-    return (
-        <div className="flex flex-col w-screen h-screen">
-            <Navbar />
-            <div className="flex flex-col items-center">
-                <h1 className="text-bold text-xl pt-4">ABOUT PLAY IT FORWARD</h1>
-                <p className="text-center pt-2">Play It Forward is an application inspired from a Hackathon project
-                    made by Jennifer Williams and Shawn Huang Fernandes. It's purpose 
-                    is to lower the barrier for volunteer engagement by gamifying the 
-                    volunteering experience. This is currently a work in progress, so stay
-                    tuned to see where it goes!
-                </p>
-                <div className="flex justify-center w-screen pt-8 pb-4">
-                    <a href=""><GifButton image={contributeImage} caption="Help Us Out"/></a>
-                    <a href=""><GifButton image={newsImage} caption="Read About PIF"/></a>
-                </div>
-                <div className="flex justify-center w-screen">
-                    <a href="https://jenniferwilliams.dev/"><GifButton image={newsImage} caption="About Jen"/></a>
-                    <a href="https://shawnhuangfernandes.netlify.com"><GifButton image={contributeImage} caption="About Shawn"/></a>
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col w-screen h-screen">
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Text Blurb */}
+      <div className="flex flex-col items-center">
+        <h1 className="text-bold text-xl pt-4">ABOUT PLAY IT FORWARD</h1>
+        <p className="text-center pt-2">
+          Play It Forward is an application inspired from a Hackathon project
+          made by Jennifer Williams and Shawn Huang Fernandes. It's purpose is
+          to lower the barrier for volunteer engagement by gamifying the
+          volunteering experience. This is currently a work in progress, so stay
+          tuned to see where it goes!
+        </p>
+
+        {/* GifButtons */}
+        <div className="flex justify-center w-screen pt-8 pb-4">
+          <a
+            href="https://github.com/Taljjaa/PlayItForward-Client"
+            target="_blank"
+          >
+            <GifButton image={contributeImage} caption="Help Us Out" />
+          </a>
+          <a
+            href="https://democracylab.ghost.io/play-it-forward-makes-volunteering-fun/"
+            target="_blank"
+          >
+            <GifButton image={newsImage} caption="Read About PIF" />
+          </a>
         </div>
-    );
+        <div className="flex justify-center w-screen">
+          <a href="https://jenniferwilliams.dev/" target="_blank">
+            <GifButton image={newsImage} caption="About Jen" />
+          </a>
+          <a href="https://shawnhuangfernandes.netlify.com" target="_blank">
+            <GifButton image={contributeImage} caption="About Shawn" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
