2a99dc20fe21b00738e2e2f4dfbd6110257bfb8c
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-03-21 12:50:54 -0700

    added text input to Login Dialogue Box with minimal styling

--- src/components/LoginDialogueBox.tsx
+++ src/components/LoginDialogueBox.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const LoginDialogueBox = () => {
     return (
-        <div className="flex flex-col h-full bg-red-800">
+        <div className="flex flex-col h-full bg-red-800 px-4 pt-8">
             
+            <input className="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg" placeholder="Enter Username"/>
         </div>
     );
 };
