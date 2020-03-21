c52162180b48617b67303b1feed771a710c72014
Taljjaa <jenken131@gmail.com>
2020-03-21 16:44:48 -0700

    added component to nonprofitdashboard, created nonprofitgraphdisplay to toggle different graph states

---
+++ src/components/NonprofitGraphDisplay.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const NonprofitGraphDisplay = () => {
+  const [displayOption, setDisplayOption] = useState('metrics');
+
+  return (
+    <div>
+      <button
+        onClick={() => setDisplayOption('metrics')}
+        type="button"
+        className="shadow bg-blue-700 hover:bg-blue-600 focus:outline-none text-white font-bold py-1 px-4 rounded">
+        Metrics
+      </button>
+      <button
+        onClick={() => setDisplayOption('events')}
+        type="button"
+        className="shadow bg-blue-700 hover:bg-blue-600 focus:outline-none text-white font-bold py-1 px-4 rounded">
+        Events
+      </button>
+      <button
+        onClick={() => setDisplayOption('volunteers')}
+        type="button"
+        className="shadow bg-blue-700 hover:bg-blue-600 focus:outline-none text-white font-bold py-1 px-4 rounded">
+        Volunteers
+      </button>
+    </div>
+  );
+};
+
+export default NonprofitGraphDisplay;
