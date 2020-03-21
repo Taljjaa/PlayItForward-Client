ba0c3846a4abadec5d5de5a5309a6e0ef12c5662
Taljjaa <jenken131@gmail.com>
2020-03-21 14:17:19 -0700

    added comments about layout, added buttons for create event and edit account, and added basic styling

--- src/pages/NonprofitDashboardPage.tsx
+++ src/pages/NonprofitDashboardPage.tsx
@@ -4,8 +4,25 @@ import React from 'react';
 //custom imports
 import NavBar from '../components/Navbar';
 
+//this functional component renders the nonprofit dashboard
+// Layout (from top --> bottom):
+//Navbar, NonprofitInfo, Create Event Button, Edit Account Button, NonprofitDataGraph
 const NonProfitDashboardPage = () => {
-  return <NavBar />;
+  return (
+    <div className="bg-blue-300 h-screen">
+      <NavBar />
+      <button
+        type="button"
+        className="shadow bg-blue-700 hover:bg-blue-600 focus:outline-none text-white font-bold py-1 px-4 rounded">
+        Create Event
+      </button>
+      <button
+        type="button"
+        className="shadow bg-blue-700 hover:bg-blue-600 focus:outline-none text-white font-bold py-1 px-4 rounded">
+        Edit Account
+      </button>
+    </div>
+  );
 };
 
 export default NonProfitDashboardPage;
