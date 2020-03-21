40af35cac2e99e97791878bd07578153b3927b68
Taljjaa <jenken131@gmail.com>
2020-03-21 13:41:04 -0700

    created nonprofit dashboard page and added route to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 // React Imports
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // Custom Imports
 import LandingPage from './pages/LandingPage';
 import EventListPage from './pages/EventListPage';
 import NonprofitListPage from './pages/NonprofitListPage';
-import AboutPage from './pages/AboutPage'
-import AuthPage from './pages/AuthPage'
+import AboutPage from './pages/AboutPage';
+import AuthPage from './pages/AuthPage';
+import NonprofitDashboardPage from './pages/NonprofitDashboardPage';
 
 //Imports from Apollo
 import ApolloClient from 'apollo-boost';
@@ -16,18 +17,18 @@ import { ApolloProvider } from '@apollo/react-hooks';
 //the client represents the endpoint to our graphql server
 
 const client = new ApolloClient({
-   uri: 'https://play-it-forward-graphql.herokuapp.com/v1/graphql',
+  uri: 'https://play-it-forward-graphql.herokuapp.com/v1/graphql',
 });
 
 //ApolloProvider allows us to access the client from anywhere in the component tree
 
 // This is the main app component
 function App() {
-   return (
-      <Router>
-         <ApolloProvider client={client}>
-            <div className="bg-blue-200 text-blue-900">
-            <Route exact path="/">
+  return (
+    <Router>
+      <ApolloProvider client={client}>
+        <div className="bg-blue-200 text-blue-900">
+          <Route exact path="/">
             <LandingPage />
           </Route>
           <Route exact path="/events">
@@ -41,11 +42,14 @@ function App() {
           </Route>
           <Route exact path="/auth">
             <AuthPage />
-          </Route>              
-          </div>
-         </ApolloProvider>
-      </Router>
-   );
+          </Route>
+          <Route path="/nonprofit-dashboard">
+            <NonprofitDashboardPage />
+          </Route>
+        </div>
+      </ApolloProvider>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/NonprofitDashboardPage.tsx
@@ -0,0 +1,11 @@
+//React imports
+import React from 'react';
+
+//custom imports
+import NavBar from '../components/Navbar';
+
+const NonProfitDashboardPage = () => {
+  return <NavBar />;
+};
+
+export default NonProfitDashboardPage;
