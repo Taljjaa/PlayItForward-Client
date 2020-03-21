0413ce95036a1063da7a280f48bc1f9e6f7b4b5d
Taljjaa <jenken131@gmail.com>
2020-03-21 16:56:33 -0700

    created NonprofitGraph to display Nonprofit information and accepts props based on the displayOption you want

--- src/components/NonprofitGraphDisplay.tsx
+++ src/components/NonprofitGraphDisplay.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import NonprofitGraph from './NonprofitGraph';
 
 const NonprofitGraphDisplay = () => {
   const [displayOption, setDisplayOption] = useState('metrics');
@@ -23,6 +24,7 @@ const NonprofitGraphDisplay = () => {
         className="shadow bg-blue-700 hover:bg-blue-600 focus:outline-none text-white font-bold py-1 px-4 rounded">
         Volunteers
       </button>
+      <NonprofitGraph displayOption={displayOption} />
     </div>
   );
 };
