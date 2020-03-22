9570ed291e33544fe396f47923498765b7b4c57c
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-03-22 14:31:27 -0700

    minor changes to dropdown menu

--- src/components/DropdownOptions.tsx
+++ src/components/DropdownOptions.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DropdownOptions = () => {
     return (
        <div className="absolute border-t border-blue-400 flex flex-col w-full h-auto py-2 bg-gray-900">
-           <a className="ml-8 text-blue-400 text-bold mb-1" href="/about">Account Settings</a>
+           <a className="ml-8 text-blue-400 text-bold mb-1" href="/settings">Account Settings</a>
            <a className="ml-8 text-blue-400 text-bold mb-1" href="/about">My Dashboard</a>
            <a className="ml-8 text-blue-400 text-bold mb-1" href="/about">My Events</a>
            <a className="ml-8 text-blue-400 text-bold" href="/about">Logout</a>
