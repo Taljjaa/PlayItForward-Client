77d8724302eafd1c2b357f9ec591b75698e27c82
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-03-22 16:56:29 -0700

    Updated Dropdown Link have functioning links with icons

--- src/components/GifButton.js
+++ src/components/GifButton.js
@@ -6,17 +6,17 @@ import React from "react";
 // Image, Caption
 const GifButton = props => {
   return (
-    <div class="flex flex-col items-center w-40 h-40 rounded shadow-lg bg-blue-400 mx-2 hover:bg-blue-500">
+    <div className="flex flex-col items-center w-40 h-40 rounded shadow-lg bg-blue-400 mx-2 hover:bg-blue-500">
       {/* Image */}
       <img
-        class="w-32 h-32 -mb-4"
+        className="w-32 h-32 -mb-4"
         src={props.image}
         alt="Sunset in the mountains"
       />
 
       {/* Caption */}
-      <div class="px-6 py-4">
-        <div class="font-bold text-sm mb-2">{props.caption}</div>
+      <div className="px-6 py-4">
+        <div className="font-bold text-sm mb-2">{props.caption}</div>
       </div>
     </div>
   );
