bb872d9088facc9fbd2f5167d67b67ce63e9194e
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-07-03 13:46:18 -0700

    Add Google Maps with API key

--- src/pages/CreateEventPage.js
+++ src/pages/CreateEventPage.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow
+} from "@react-google-maps/api";
 const CreateEventPage = () => {
   const [eventName, setEventName] = useState("");
   const [date, setDate] = useState("");
@@ -11,15 +16,34 @@ const CreateEventPage = () => {
     // Add add routing logic here
   };
 
+  const libraries = ["places"];
+  const mapContainerStyle = {
+    height: "100vh",
+    width: "100%"
+  };
+
+  const center = {
+    lat: 43.6532,
+    lng: -79.3832
+  };
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries
+  });
+
+  if (loadError) return null;
+  if (!isLoaded) return null;
+
   return (
     <div className="flex h-screen">
-      <div className="flex flex-col justify-around items-center h-full w-104 bg-red-400 px-4">
+      <div className="flex flex-col justify-around items-center h-full w-104 px-4">
         <input
           className="text-center w-full text-white bg-blue-800 focus:outline-none focus:shadow-outline border border-blue-500"
           type="text"
           placeholder="Enter Event Name Here"
           value={eventName}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          onChange={e => {
             e.persist();
             setEventName(e.target.value);
           }}
@@ -30,7 +54,7 @@ const CreateEventPage = () => {
           type="text"
           placeholder="Enter Date Here - MM/DD/YY"
           value={date}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          onChange={e => {
             e.persist();
             setDate(e.target.value);
           }}
@@ -41,7 +65,7 @@ const CreateEventPage = () => {
           type="text"
           placeholder="Enter Event Location Here"
           value={location}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          onChange={e => {
             e.persist();
             setLocation(e.target.value);
           }}
@@ -54,7 +78,14 @@ const CreateEventPage = () => {
           Register
         </button>
       </div>
-      <div className="h-full flex bg-red-300"></div>
+      <div className="h-full w-full">
+        <GoogleMap
+          id="map"
+          mapContainerStyle={mapContainerStyle}
+          zoom={8}
+          center={center}
+        ></GoogleMap>
+      </div>
     </div>
   );
 };
