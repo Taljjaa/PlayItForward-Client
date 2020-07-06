00506263b388909696d8c5a4a384a87913f4a792
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-07-06 10:04:31 -0700

    Add Location Pins on map

--- src/pages/CreateEventPage.js
+++ src/pages/CreateEventPage.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import {
   GoogleMap,
   useLoadScript,
   Marker,
   InfoWindow
 } from "@react-google-maps/api";
+
+import { mapStyles } from "../media/mapStyles/mapStyles";
+import { assertTSPropertySignature } from "babel-types";
+
 const CreateEventPage = () => {
   const [eventName, setEventName] = useState("");
   const [date, setDate] = useState("");
@@ -16,6 +20,19 @@ const CreateEventPage = () => {
     // Add add routing logic here
   };
 
+  const [marker, setMarker] = useState({});
+  const onMapClick = React.useCallback(event => {
+    setMarker({
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng()
+    });
+  }, []);
+
+  const mapRef = useRef();
+  const onMapLoad = useCallback(map => {
+    mapRef.current = map;
+  });
+
   const libraries = ["places"];
   const mapContainerStyle = {
     height: "100vh",
@@ -23,8 +40,14 @@ const CreateEventPage = () => {
   };
 
   const center = {
-    lat: 43.6532,
-    lng: -79.3832
+    lat: 47.67805,
+    lng: -122.338482
+  };
+
+  const options = {
+    styles: mapStyles,
+    disableDefaultUI: assertTSPropertySignature,
+    zoomControl: true
   };
 
   const { isLoaded, loadError } = useLoadScript({
@@ -82,9 +105,24 @@ const CreateEventPage = () => {
         <GoogleMap
           id="map"
           mapContainerStyle={mapContainerStyle}
-          zoom={8}
+          zoom={15}
           center={center}
-        ></GoogleMap>
+          options={options}
+          onClick={event => onMapClick(event)}
+          onLoad={onMapLoad}
+        >
+          <Marker
+            key={Math.random()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            // icon= {{
+            //   url: 'directory/',
+            //   scaledSize: new window.google.maps.Size(30, 30),
+            //   origin: new window.google.maps.Point(0,0),
+            //   anchor: new window.google.maps.Point(15, 15)
+
+            // }}
+          />
+        </GoogleMap>
       </div>
     </div>
   );
