f7b42017b6ed0ac937ee197733b3804a64b1a7d7
Taljjaa <jenken131@gmail.com>
2020-07-08 13:42:18 -0700

    converted login form to react-hook-form

--- src/components/LoginDialogueBox.tsx
+++ src/components/LoginDialogueBox.tsx
@@ -1,12 +1,18 @@
-import React, { useState, SyntheticEvent } from "react";
-import { useHistory } from "react-router-dom";
-import { useMutation, useApolloClient } from "@apollo/react-hooks";
-import gql from "graphql-tag";
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useMutation, useApolloClient } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { useForm } from 'react-hook-form';
 
 // Type definitions
 type DialogueProps = {
   isVolunteer: boolean;
   setIsVolunteer: any;
+};
+
+type Inputs = {
+  username: string;
+  password: string;
 };
 
 const LOGIN_VOLUNTEER = gql`
@@ -44,8 +50,7 @@ const LOGIN_NONPROFIT = gql`
 // Structure:
 // Header, Text Fields, Radio Selection, Submit Button
 const LoginDialogueBox = (props: DialogueProps) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const { register, handleSubmit, errors } = useForm<Inputs>();
 
   //hook used to access the cache
   const client = useApolloClient();
@@ -54,18 +59,13 @@ const LoginDialogueBox = (props: DialogueProps) => {
   const [loginVol] = useMutation(LOGIN_VOLUNTEER);
   const [loginNonprofit] = useMutation(LOGIN_NONPROFIT);
 
-  const onChangeUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.persist();
-    setUsername(e.target.value);
-  };
-
-  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.persist();
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
+  const onSubmit = async ({
+    username,
+    password,
+  }: {
+    username: string;
+    password: string;
+  }) => {
     if (props.isVolunteer) {
       let volunteerData = await loginVol({ variables: { username, password } });
       console.log(volunteerData);
@@ -73,33 +73,32 @@ const LoginDialogueBox = (props: DialogueProps) => {
         client.writeData({
           data: {
             volunteerID: volunteerData.data.loginVolunteer.volunteer.id,
-            token: volunteerData.data.loginVolunteer.token
-          }
+            token: volunteerData.data.loginVolunteer.token,
+          },
         });
-        history.push("/volunteer-dashboard");
+        history.push('/volunteer-dashboard');
       }
     } else {
       let nonprofitData = await loginNonprofit({
-        variables: { username, password }
+        variables: { username, password },
       });
 
       if (nonprofitData.data.loginNonprofit.ok) {
         client.writeData({
           data: {
             nonprofitID: nonprofitData.data.loginNonprofit.nonprofit.id,
-            token: nonprofitData.data.loginNonprofit.token
-          }
+            token: nonprofitData.data.loginNonprofit.token,
+          },
         });
-        history.push("/nonprofit-dashboard");
+        history.push('/nonprofit-dashboard');
       }
     }
   };
 
   return (
     <form
-      onSubmit={handleSubmit}
-      className="bg-blue-500 flex flex-col h-full w-full px-4 pt-4"
-    >
+      onSubmit={handleSubmit(onSubmit)}
+      className="bg-blue-500 flex flex-col h-full w-full px-4 pt-4">
       {/* Header */}
       <p className="text-center text-white font-semibold text-xl pb-4">
         Welcome Back!
@@ -107,43 +106,24 @@ const LoginDialogueBox = (props: DialogueProps) => {
 
       {/* Text Fields */}
       <input
-        className="text-center text-white bg-blue-800 focus:outline-none focus:shadow-outline border border-blue-500 mb-2"
+        name="username"
+        ref={register({ required: true })}
         placeholder="Enter Username"
-        value={username}
-        onChange={onChangeUsername}
-      />
-      <input
         className="text-center text-white bg-blue-800 focus:outline-none focus:shadow-outline border border-blue-500 mb-2"
-        type="password"
-        placeholder="Enter Password"
-        value={password}
-        onChange={onChangePassword}
       />
-
-      {/* Radio Selection */}
-      <div className="flex justify-around text-white">
-        <label className="inline-flex items-center">
-          <input
-            type="radio"
-            className="form-radio"
-            name="accountType"
-            checked={props.isVolunteer}
-            onClick={() => props.setIsVolunteer(true)}
-          />
-          <span className="ml-2">Volunteer</span>
-        </label>
-        <label className="inline-flex items-center">
-          <input
-            type="radio"
-            className="form-radio"
-            name="accountType"
-            checked={!props.isVolunteer}
-            onClick={() => props.setIsVolunteer(false)}
-          />
-          <span className="ml-2">Nonprofit</span>
-        </label>
-      </div>
-
+      {errors.username && (
+        <span className="text-white">Username is required</span>
+      )}
+      <input
+        name="password"
+        type="password"
+        ref={register({ required: true })}
+        placeholder="Enter Password"
+        className="text-center text-white bg-blue-800 focus:outline-none focus:shadow-outline border border-blue-500 mb-2"
+      />
+      {errors.password && (
+        <span className="text-white">Password is required</span>
+      )}
       {/* Submit Button */}
       <div className="flex justify-center">
         <button className="bg-blue-600 w-24 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded mt-4 mb-2">
