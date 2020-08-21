10f05918b705d43562a9069af116592f7ce815d0
Taljjaa <jenken131@gmail.com>
2020-08-21 13:49:08 -0700

    added another color variable and comment to easily convert from tailwind to hex, and converted DropdownOptions from Tailwind to Sass

--- src/components/DropdownOptions.tsx
+++ src/components/DropdownOptions.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import './DropdownOptions.scss';
 
 const DropdownOptions = () => {
   const history = useHistory();
@@ -20,37 +21,33 @@ const DropdownOptions = () => {
   };
 
   return (
-    <div className="absolute border-t border-blue-400 flex flex-col h-auto px-6 py-2 bg-gray-900 w-full">
-      <button className="flex px-2 bg-gray-900 py-2 hover:bg-blue-700 rounded-lg">
+    <div className="dropdownContainer">
+      <button className="dropdownButton">
         <svg
-          className="fill-current text-blue-500 h-6 w-6"
+          className="dropdownIcon"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20">
           <path d="M3.94 6.5L2.22 3.64l1.42-1.42L6.5 3.94c.52-.3 1.1-.54 1.7-.7L9 0h2l.8 3.24c.6.16 1.18.4 1.7.7l2.86-1.72 1.42 1.42-1.72 2.86c.3.52.54 1.1.7 1.7L20 9v2l-3.24.8c-.16.6-.4 1.18-.7 1.7l1.72 2.86-1.42 1.42-2.86-1.72c-.52.3-1.1.54-1.7.7L11 20H9l-.8-3.24c-.6-.16-1.18-.4-1.7-.7l-2.86 1.72-1.42-1.42 1.72-2.86c-.3-.52-.54-1.1-.7-1.7L0 11V9l3.24-.8c.16-.6.4-1.18.7-1.7zM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
         </svg>
-        <span className="ml-2 text-blue-400 font-semibold">Settings</span>
+        <span className="dropdownText">Settings</span>
       </button>
-      <button
-        onClick={handleDashboard}
-        className="flex px-2 bg-gray-900 py-2 hover:bg-blue-700 rounded-lg">
+      <button onClick={handleDashboard} className="dropdownButton">
         <svg
-          className="fill-current text-blue-500 h-6 w-6"
+          className="dropdownIcon"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20">
           <path d="M4.13 12H4a2 2 0 1 0 1.8 1.11L7.86 10a2.03 2.03 0 0 0 .65-.07l1.55 1.55a2 2 0 1 0 3.72-.37L15.87 8H16a2 2 0 1 0-1.8-1.11L12.14 10a2.03 2.03 0 0 0-.65.07L9.93 8.52a2 2 0 1 0-3.72.37L4.13 12zM0 4c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4z" />
         </svg>
-        <span className="ml-2 text-blue-400 font-semibold">Dashboard</span>
+        <span className="dropdownText">Dashboard</span>
       </button>
-      <button
-        onClick={handleLogout}
-        className="flex px-2 bg-gray-900 py-2 hover:bg-blue-700 rounded-lg">
+      <button onClick={handleLogout} className="dropdownButton">
         <svg
-          className="fill-current text-blue-500 h-6 w-6"
+          className="dropdownIcon"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20">
           <path d="M10 0a10 10 0 1 1 0 20 10 10 0 0 1 0-20zM2 10a8 8 0 1 0 16 0 8 8 0 0 0-16 0zm10.54.7L9 14.25l-1.41-1.41L10.4 10 7.6 7.17 9 5.76 13.24 10l-.7.7z" />
         </svg>
-        <span className="ml-2 text-blue-400 font-semibold">Logout</span>
+        <span className="dropdownText">Logout</span>
       </button>
     </div>
   );
