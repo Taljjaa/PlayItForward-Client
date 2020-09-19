3a7e4bbe7fd9ad40e331274b5a89eceb833f9551
Taljjaa <jenken131@gmail.com>
2020-09-19 11:56:24 -0700

    AboutPage Sass rewrite

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -13,51 +13,49 @@ import './AboutPage.scss';
 // Navbar, Text Blurb, GifButtons!
 const AboutPage = () => {
   return (
-    <div>
+    <div className="aboutPageContainer">
       {/* Navbar */}
       <Navbar />
 
       {/* Text Blurb */}
-      <div>
-        <h1 className="text-center font-bold text-4xl pt-4">
-          ABOUT PLAY IT FORWARD
-        </h1>
-        <p className="w-8/12 text-xl xl:text-2xl text-center pt-2">
+      <div className="aboutPageText">
+        <h1 className="aboutPageHeader">ABOUT PLAY IT FORWARD</h1>
+        <p className="aboutPageParagraph">
           Play It Forward is an application made to get volunteers excited about
           volunteering by gamifying the experience while also providing a
           platform for nonprofits to get the support they need to build and
           support their communities.
         </p>
+      </div>
 
-        {/* GifButtons */}
-        <div className="flex justify-center w-screen pt-4 pb-2">
-          <a
-            href="https://github.com/Taljjaa/PlayItForward-Client"
-            target="_blank"
-            rel="noopener noreferrer">
-            <GifButton image={contributeImage} caption="Help Us Out" />
-          </a>
-          <a
-            href="https://democracylab.ghost.io/play-it-forward-makes-volunteering-fun/"
-            target="_blank"
-            rel="noopener noreferrer">
-            <GifButton image={newsImage} caption="Read About PIF" />
-          </a>
-        </div>
-        <div className="flex justify-center w-screen pt-4 pb-2">
-          <a
-            href="https://jenniferwilliams.dev/"
-            target="_blank"
-            rel="noopener noreferrer">
-            <GifButton image={newsImage} caption="About Jen" />
-          </a>
-          <a
-            href="https://shawnhuangfernandes.netlify.com"
-            target="_blank"
-            rel="noopener noreferrer">
-            <GifButton image={contributeImage} caption="About Shawn" />
-          </a>
-        </div>
+      {/* GifButtons */}
+      <div className="aboutPageGifButtonContainer">
+        <a
+          href="https://github.com/Taljjaa/PlayItForward-Client"
+          target="_blank"
+          rel="noopener noreferrer">
+          <GifButton image={contributeImage} caption="Help Us Out" />
+        </a>
+        <a
+          href="https://democracylab.ghost.io/play-it-forward-makes-volunteering-fun/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <GifButton image={newsImage} caption="Read About PIF" />
+        </a>
+      </div>
+      <div className="aboutPageGifButtonContainer">
+        <a
+          href="https://jenniferwilliams.dev/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <GifButton image={newsImage} caption="About Jen" />
+        </a>
+        <a
+          href="https://shawnhuangfernandes.netlify.com"
+          target="_blank"
+          rel="noopener noreferrer">
+          <GifButton image={contributeImage} caption="About Shawn" />
+        </a>
       </div>
     </div>
   );
