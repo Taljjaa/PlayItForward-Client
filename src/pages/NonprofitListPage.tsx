88073b729e3108c7c6cfae47cdbd7eeb2583b8d0
Taljjaa <jenken131@gmail.com>
2020-09-19 12:01:39 -0700

    NonprofitListPage Sass rewrite

--- src/pages/NonprofitListPage.tsx
+++ src/pages/NonprofitListPage.tsx
@@ -5,13 +5,14 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import NonprofitSearchFilter from '../components/NonprofitSearchFilter';
 import NonprofitCardCollection from '../components/NonprofitCardCollection';
+import './NonprofitListPage.scss';
 
 // Function component for the nonprofit list page
 // Layout
 // Navbar, NonprofitSearchFilter, NonprofitCardCollection
 const NonprofitListPage = () => {
   return (
-    <div className="nonprofit-list-pg flex flex-col w-screen h-screen">
+    <div className="nonprofitListPageContainer ">
       <Navbar />
       <NonprofitSearchFilter />
       <NonprofitCardCollection />
